0ef25e0bdaf6bfd748373e2dedb05f2f7e58f39d
Sven Hertle <sven.hertle@googlemail.com>
2015-12-20 16:33:50 +0100

    model for semester

--- settool_common/admin.py
+++ settool_common/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Semester
+
+admin.site.register(Semester)
--- settool_common/models.py
+++ settool_common/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Semester(models.Model):
+    class Meta:
+        unique_together = (('semester', 'year'),)
+        ordering = ['year', 'semester']
+
+    WINTER = 'WS'
+    SUMMER = 'SS'
+    SEMESTER_CHOICES = (
+        (WINTER, _('Winter semester')),
+        (SUMMER, _('Summer semester')),
+    )
+
+    semester = models.CharField(
+        max_length=2,
+        choices=SEMESTER_CHOICES,
+        default=WINTER,
+        verbose_name=_("Semester"),
+    )
+
+    year = models.PositiveIntegerField(
+        verbose_name=_("Year"),
+    )
+
+    def __str__(self):
+        return "{} {:4}".format(self.get_semester_display(), self.year)
