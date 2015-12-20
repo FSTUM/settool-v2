04412c3741217a1ad7d65191892d77b627367ee3
Sven Hertle <sven.hertle@googlemail.com>
2015-12-20 17:14:25 +0100

    added models for guidedtours
    not finished yet, name and description will be there in different
    languages (see django-modeltranslation)

--- guidedtours/admin.py
+++ guidedtours/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tour, Participant
+
+admin.site.register(Tour)
+admin.site.register(Participant)
--- guidedtours/models.py
+++ guidedtours/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Tour(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name=_("Name"),
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name=_("Description"),
+    )
+
+    date = models.DateTimeField(
+        verbose_name=_("Date"),
+    )
+
+    def __str__(self):
+        return self.name
+
+class Participant(models.Model):
+    tour = models.ForeignKey(
+        Tour
+    )
+
+    firstname = models.CharField(
+        max_length=200,
+        verbose_name=_("First name"),
+    )
+
+    surname = models.CharField(
+        max_length=200,
+        verbose_name=_("Surname"),
+    )
+
+    email = models.EmailField(
+        verbose_name=_("E-Mail"),
+    )
+
+    phone = models.CharField(
+        max_length=200,
+        verbose_name=_("Mobile phone"),
+    )
+
+    def __str__(self):
+        return "{} {}".format(self.firstname, self.surname)
