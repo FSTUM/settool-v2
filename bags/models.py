0aae02a94baf672396d81e3d6ebf717c5b9003c4
Julian Biendarra <biendarra@fs.tum.de>
2016-05-26 11:00:57 +0200

    add model for company in bags

--- bags/models.py
+++ bags/models.py
@@ -1,5 +1,66 @@
 from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 
 from django.db import models
 
-# Create your models here.
+from settool_common.models import Semester
+
+class Company(models.Model):
+    semester = models.ForeignKey(
+        Semester
+    )
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name=_("Name"),
+    )
+
+    contact = models.CharField(
+        max_length=200,
+        verbose_name=_("Contact person"),
+    )
+
+    email = models.CharField(
+        max_length=200,
+        verbose_name=_("Email address"),
+    )
+
+    email_sent = models.BooleanField(
+        _("Email sent"),
+    )
+
+    email_sent_success = models.BooleanField(
+        _("Email successfully sent"),
+    )
+
+    zusage = models.NullBooleanField(
+        _("Zusage"),
+    )
+
+    giveaways = models.CharField(
+        max_length=200,
+        verbose_name=_("Giveaways"),
+    )
+
+    arrival_time = models.CharField(
+        max_length=200,
+        verbose_name=_("Arrival time"),
+    )
+
+    comment = models.CharField(
+        max_length=200,
+        verbose_name=_("Comment"),
+    )
+
+    last_year = models.BooleanField(
+        _("Participated last year"),
+    )
+
+    arrived = models.BooleanField(
+        _("Arrived"),
+    )
+
+    contact_again = models.NullBooleanField(
+        _("Contact again"),
+    )
+
