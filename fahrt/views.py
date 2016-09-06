80f392b50b994900388ef3635571ae12fb8ceaa2
Julian Biendarra <biendarra@fs.tum.de>
2016-09-06 15:03:34 +0200

    add import of permission_required

--- fahrt/views.py
+++ fahrt/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
 
 from . models import Participant
 from .forms import ParticipantForm
