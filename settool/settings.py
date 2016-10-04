41d39bfea3f0dc29c28ab12a4c5c17daaa504c1b
Julian Biendarra <biendarra@fs.tum.de>
2016-10-04 15:25:21 +0200

    add django mailman app

--- settool/settings.py
+++ settool/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'django_mailman',
     'settool_common',
     'guidedtours',
     'bags',
