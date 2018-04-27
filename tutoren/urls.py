b09a9e6dc2bd4e7f4d749e7b6eea71716c45cf84
Dennis Fischer <fischerd@fs.tum.de>
2018-04-27 20:02:58 +0200

    Added option to list, edit, and view tutors

--- tutoren/urls.py
+++ tutoren/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('confirm/', views.tutoren_signup_confirmation_required, name='tutoren_signup_confirmation_required'),
     path('invalid/', views.tutoren_signup_invalid, name='tutoren_signup_invalid'),
     path('confirm/<uidb64>/<token>/', views.tutoren_signup_confirm, name='tutoren_signup_confirm'),
+    path('list/', views.tutoren_list, name='tutoren_list'),
+    path('list/<str:status>/', views.tutoren_list, name='tutoren_list_status'),
+    path('view/<uuid:uid>/', views.tutoren_view, name='tutoren_view'),
+    path('accept/<uuid:uid>/', views.tutoren_accept, name='tutoren_accept'),
+    path('decline/<uuid:uid>/', views.tutoren_decline, name='tutoren_decline'),
+    path('delete/<uuid:uid>/', views.tutoren_delete, name='tutoren_delete'),
+    path('edit/<uuid:uid>/', views.tutoren_edit, name='tutoren_edit'),
 ]
--- tutoren/views.py
+++ tutoren/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.decorators import permission_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from common.models import get_semester, Semester
-from tutoren.forms import TutorenForm
+from tutoren.forms import TutorenForm, TutorenAdminForm
 from tutoren.models import Tutor, Status
 from tutoren.tokens import account_activation_token
 
@@ -74,3 +76,57 @@ def tutoren_signup_confirm(request, uidb64, token):
         return redirect('tutoren_signup_success')
     else:
         return redirect('tutoren_signup_invalid')
+
+
+@permission_required('tutoren.edit_tutors')
+def tutoren_list(request, status=None):
+    if status is None:
+        tutors = Tutor.objects.all()
+    else:
+        tutors = Tutor.objects.filter(status__key=status)
+    return render(request, 'tutoren/list_tutors.html', {'tutors': tutors})
+
+
+def tutoren_view(request, uid):
+    tutor = get_object_or_404(Tutor, pk=uid)
+    return render(request, 'tutoren/view_tutor.html', {'tutor': tutor})
+
+
+@permission_required('tutoren.edit_tutors')
+def tutoren_accept(request, uid):
+    tutor = get_object_or_404(Tutor, pk=uid)
+    tutor.status = Status.objects.get(key='accepted')
+    tutor.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+@permission_required('tutoren.edit_tutors')
+def tutoren_decline(request, uid):
+    tutor = get_object_or_404(Tutor, pk=uid)
+    tutor.status = Status.objects.get(key='declined')
+    tutor.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+@permission_required('tutoren.edit_tutors')
+def tutoren_delete(request, uid):
+    tutor = get_object_or_404(Tutor, pk=uid)
+    tutor.delete()
+    return redirect("tutoren_list")
+
+
+@permission_required('tutoren.edit_tutors')
+def tutoren_edit(request, uid):
+    tutor = get_object_or_404(Tutor, pk=uid)
+
+    form = TutorenAdminForm(request.POST or None, semester=tutor.semester, instance=tutor)
+    if form.is_valid():
+        form.save()
+        tutor.log(request.user, "Tutor edited")
+
+        return redirect('tutoren_view', tutor.id)
+
+    return render(request, 'tutoren/edit_tutor.html', {
+        'form': form,
+        'tutor': tutor,
+    })
