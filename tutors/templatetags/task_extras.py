3d69e1d2a57edc6ffe1cc369fd4965efb595b6f9
Dennis Fischer <fischerd@fs.tum.de>
2018-06-24 01:01:24 +0200

    Added # of missing mails.

--- tutors/templatetags/task_extras.py
+++ tutors/templatetags/task_extras.py
@@ -1,8 +1,16 @@
 from django import template
 
+from tutors.models import Tutor, MailTutorTask
+
 register = template.Library()
 
 
 @register.filter
 def in_task(requirement, task_requirements):
     return requirement.filter(question__id__in=task_requirements.values("id"))
+
+
+@register.simple_tag
+def mail_task_count(task):
+    return Tutor.objects.filter(task=task).exclude(id__in=MailTutorTask.objects.filter(task=task).values(
+        "tutor_id")).count()
