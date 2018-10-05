f0899a3fde4790406245e99402b844d2d720f64d
Benedikt Seidl <benedikt.seidl@tum.de>
2018-10-06 00:44:41 +0200

    Escape Latex strings properly

---
+++ settool_common/templatetags/latex.py
@@ -0,0 +1,25 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def latex_escape(string):
+    string = str(string)
+
+    print(string)
+
+    string = string.replace('\\', '\\textbackslash ')
+    string = string.replace(' ', '\\ ')
+    string = string.replace('&', '\\&')
+    string = string.replace('%', '\\%')
+    string = string.replace('$', '\\$')
+    string = string.replace('#', '\\#')
+    string = string.replace('_', '\\_')
+    string = string.replace('{', '\\{')
+    string = string.replace('}', '\\}')
+    string = string.replace('~', '\\textasciitilde ')
+    string = string.replace('^', '\\textasciicircum ')
+
+    print(string)
+
+    return '{' + string + '}'
