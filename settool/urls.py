e819df8c29b65b0ddffe123289464856fe34f1a0
Frank <frank@elsinga.de>
2020-11-25 09:01:16 +0100

    refactored URLs for code-readability

--- settool/urls.py
+++ settool/urls.py
@@ -22,39 +22,32 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-                  # admin
-                  url(r'^admin/', admin.site.urls),
+    # admin
+    url(r'^admin/', admin.site.urls),
 
-                  # login, logout
-                  url(r'^login/$',
-                      auth_views.LoginView.as_view(template_name='settool_common/login.html'),
-                      # {'template_name': 'settool_common/login.html'},
-                      name='login'),
+    # login, logout
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='settool_common/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
-                  url(r'^logout/$',
-                      auth_views.LogoutView.as_view(next_page='/'),
-                      # {'next_page': '/'},
-                      name='logout'),
+    # localization
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 
-                  # localization
-                  url(r'^i18n/', include('django.conf.urls.i18n')),
+    # index
+    url(r'^$', TemplateView.as_view(template_name="base.html")),
 
-                  # index
-                  url(r'^$', TemplateView.as_view(template_name="base.html")),
+    # settool_common: choose semester
+    url(r'^semester/', include('settool_common.urls')),
 
-                  # settool_common: choose semester
-                  url(r'^semester/', include('settool_common.urls')),
+    # guided tours
+    url(r'^tours/', include('guidedtours.urls')),
 
-                  # guided tours
-                  url(r'^tours/', include('guidedtours.urls')),
+    # freshmen bags
+    url(r'^bags/', include('bags.urls')),
 
-                  # freshmen bags
-                  url(r'^bags/', include('bags.urls')),
+    # SET-Fahrt
+    url(r'^fahrt/', include('fahrt.urls')),
 
-                  # SET-Fahrt
-                  url(r'^fahrt/', include('fahrt.urls')),
-
-                  # Tutoren
-                  url(r'^tutors/', include('tutors.urls')),
-
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Tutoren
+    url(r'^tutors/', include('tutors.urls')),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
