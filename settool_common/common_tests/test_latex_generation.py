3f38ebe15695796ecaf640900df9df33d19f3d8e
Frank Elsinga <frank@elsinga.de>
2020-12-13 00:06:19 +0100

    added test for escaping latex correctly

---
+++ settool_common/common_tests/test_latex_generation.py
@@ -0,0 +1,25 @@
+import unittest
+
+from settool_common.templatetags import latex
+
+
+class LatexAuxTest(unittest.TestCase):
+    def test_escape_latex_empty(self):
+        self.assertEqual("{}", latex.latex_escape(""))
+
+    def test_escape_latex_replacements(self):
+        replacements = [
+            (" ", "\\ "),
+            ("&", "\\&"),
+            ("%", "\\%"),
+            ("$", "\\$"),
+            ("#", "\\#"),
+            ("_", "\\_"),
+            ("{", "\\{"),
+            ("}", "\\}"),
+            ("\\", "\\textbackslash "),
+            ("~", "\\textasciitilde "),
+            ("^", "\\textasciicircum "),
+        ]
+        for input_str, replacement_str in replacements:
+            self.assertEqual(f"{{{replacement_str}}}", latex.latex_escape(input_str))
