d179c66873fcdaeab8978f610488d4997b6ebd5a
Frank Elsinga <frank@elsinga.de>
2020-12-17 17:33:50 +0100

    added LOCALE_PATH to include the translations for the main template in the final website

--- settool/settings.py
+++ settool/settings.py
@@ -117,23 +117,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = "en"
-
 LANGUAGES = (
     ("de", _("German")),
     ("en", _("English")),
 )
-
+TIME_ZONE = "Europe/Berlin"
+USE_I18N = True
+USE_L10N = True
+LOCALE_PATHS = ["locale"]
 MODELTRANSLATION_TRANSLATION_FILES = ("tutors.translation",)
 
 BOOTSTRAP4 = {
     "include_jquery": False,
 }
-
-TIME_ZONE = "Europe/Berlin"
-
-USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
