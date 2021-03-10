5a7256c1aa68f9c2f53700642374491e63ac40ec
Frank Elsinga <frank@elsinga.de>
2021-03-10 14:13:05 +0100

    fixed failing test

--- settool_common/common_tests/test_latex_generation.py
+++ settool_common/common_tests/test_latex_generation.py
@@ -5,7 +5,7 @@ from settool_common.templatetags import latex
 
 class LatexAuxTest(unittest.TestCase):
     def test_escape_latex_empty(self):
-        self.assertEqual("{}", latex.latex_escape(""))
+        self.assertEqual("", latex.latex_escape(""))
 
     def test_escape_latex_replacements(self):
         replacements = [
