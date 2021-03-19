1b5535df19072f9043202cec3bc2f3112fddd312
Frank Elsinga <frank@elsinga.de>
2021-03-19 22:44:13 +0100

    changed fixtures to new standard

--- settool_common/fixtures/test_fixture.py
+++ settool_common/fixtures/test_fixture.py
@@ -2,16 +2,28 @@ import random
 
 from django.utils.datetime_safe import datetime
 
-from settool_common.models import current_semester, Semester, Subject
+from settool_common.models import CourseBundle, current_semester, Semester, Subject
 from tutors.models import Tutor
 
 
 def create_tutor_fixture_state():
+    course_bundle_mathe = CourseBundle.objects.create(name="Mathe", name_de="Mathematics", name_en="Mathematik")
+    course_bundle_info = CourseBundle.objects.create(name="Info", name_de="Informatics", name_en="Informatik")
+    subject1 = Subject.objects.create(
+        degree=Subject.BACHELOR,
+        subject="Info",
+        subject_de="Informathik",
+        subject_en="Informathics",
+        course_bundle=course_bundle_info,
+    )
+    subject2 = Subject.objects.create(
+        degree=Subject.MASTER,
+        subject="Mathe",
+        subject_de="Mathemathik",
+        subject_en="mathemathics",
+        course_bundle=course_bundle_mathe,
+    )
     semester1, semester2 = generate_semesters()
-    subject1 = Subject(degree=Subject.BACHELOR, subject=Subject.INFORMATICS)
-    subject1.save()
-    subject2 = Subject(degree=Subject.MASTER, subject=Subject.MATHEMATICS)
-    subject2.save()
 
     for i in range(20):
         tutor = Tutor(  # nosec: this is a fixture
