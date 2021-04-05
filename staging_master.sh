11edb29048e4d07548de5f54616bedd53f2d48a8
Frank Elsinga <frank@elsinga.de>
2021-04-05 19:59:53 +0200

    refactored the staging setup to have better and most importantly more reliable stagings. Made staging messages less verbose

--- staging_master.sh
+++ staging_master.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 while true; do 
-if git pull --dry-run | grep -q -v 'Already up-to-date.' && changed=1
-then
-sh staging.sh
-fi
-sleep 2m
+  git fetch origin
+  reslog=$(git log HEAD..origin/main --oneline)
+  if [ "${reslog}" != "" ] ; then
+    git merge origin/master # completing the pull
+    echo "last Run:"
+    date
+    sh staging.sh
+  fi
+  sleep 1m
 done
