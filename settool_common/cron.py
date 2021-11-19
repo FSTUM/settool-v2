cb54bf415a26fcd95a99114871eb04ff03ba99d8
Frank Elsinga <frank@elsinga.de>
2021-11-19 02:04:57 +0100

    implemented the anonymization methods

--- settool_common/cron.py
+++ settool_common/cron.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from typing import Any, Optional, Union
 
 from django.db.models.query_utils import Q
+from django.utils import timezone
 from django.utils.datetime_safe import date, datetime
 
 import fahrt
@@ -89,29 +90,72 @@ def date_is_too_old(date_obj: Union[datetime, date, real_datetime.datetime, real
     return date.today() + graceperiod <= date_obj
 
 
-def anonymise_fahrt(semester: Semester, today: date, log_name: str) -> bool:
-    current_fahrt: Optional[Fahrt] = get_or_none(Fahrt, semester=semester)
-    if current_fahrt and date_is_too_old(today, current_fahrt.date, log_name):
-        # fahrt is save to be anonymised for this semester
-        # TODO
+def anonymize_fahrt(semester: Semester, log_name: str) -> bool:
+    current_fahrt: Optional[m_fahrt.Fahrt] = get_or_none(m_fahrt.Fahrt, semester=semester)
+    if current_fahrt and date_is_too_old(current_fahrt.date, log_name):
+        # fahrt is save to anonymize for this semester
+        m_fahrt.Transportation.objects.filter(fahrt__semester=semester).delete()
+        m_fahrt.TransportationComment.objects.filter(sender__semester=semester).delete()
+        semester_participants = m_fahrt.Participant.objects.filter(semester=semester)
+        semester_participants.exclude(status=m_fahrt.Participant.STATUS_CONFIRMED).delete()
+        participant: m_fahrt.Participant
+        for participant in semester_participants.all():
+            participant.registration_time = timezone.now()
+            participant.firstname = f"f {participant.pk}"
+            participant.surname = f"l {participant.pk}"
+            participant.birthday = timezone.now().date()
+            participant.email = f"{participant.pk}@example.com"
+            participant.phone = ""
+            participant.mobile = ""
+            participant.allergies = ""
+            participant.publish_contact_to_other_paricipants = False
+            participant.mailinglist = False
+            participant.comment = ""
+            participant.save()
         return True
     return False
 
 
-def anonymise_guidedtours(semester: Semester, today: date, log_name: str) -> bool:
-    most_recent_tour: Optional[Tour] = Tour.objects.filter(semester=semester).order_by("date").last()
-    if most_recent_tour and date_is_too_old(today, most_recent_tour.date, log_name):
-        # guidedtours is save to be anonymised for this semester
-        # TODO
+def anonymize_guidedtours(semester: Semester, log_name: str) -> bool:
+    most_recent_tour: Optional[m_guidedtours.Tour] = (
+        m_guidedtours.Tour.objects.filter(semester=semester)
+        .order_by(
+            "date",
+        )
+        .last()
+    )
+    if most_recent_tour and date_is_too_old(most_recent_tour.date, log_name):
+        # guidedtours is save to anonymize for this semester
+        participant: m_guidedtours.Participant
+        for participant in m_guidedtours.Participant.objects.filter(tour__semester=semester).all():
+            participant.firstname = f"f {participant.pk}"
+            participant.surname = f"l {participant.pk}"
+            participant.email = f"{participant.pk}@example.com"
+            participant.phone = f"-1 000 {participant.pk}"
+            participant.time = timezone.now()
+            participant.save()
         return True
     return False
 
 
-def anonymise_tutors(semester: Semester, today: date, log_name: str) -> bool:
-    most_recent_task: Optional[Task] = Task.objects.filter(semester=semester).order_by("end").last()
-    if most_recent_task and date_is_too_old(today, most_recent_task.end, log_name):
-        # guidedtours is save to be anonymised for this semester
-        # TODO
+def anonymize_tutors(semester: Semester, log_name: str) -> bool:
+    most_recent_task: Optional[m_tutors.Task] = m_tutors.Task.objects.filter(semester=semester).order_by("end").last()
+    if most_recent_task and date_is_too_old(most_recent_task.end, log_name):
+        # guidedtours is save to anonymize for this semester
+        m_tutors.Answer.objects.filter(tutor__semester=semester).delete()
+        m_tutors.MailTutorTask.objects.filter(tutor__semester=semester).delete()
+        semester_tutors = m_tutors.Tutor.objects.filter(semester=semester)
+        semester_tutors.exclude(status__in=[m_tutors.Tutor.STATUS_ACCEPTED, m_tutors.Tutor.STATUS_EMPLOYEE]).delete()
+        tutor: m_tutors.Tutor
+        for tutor in semester_tutors.all():
+            tutor.first_name = f"f {tutor.pk}"
+            tutor.last_name = f"l {tutor.pk}"
+            tutor.email = f"{tutor.pk}@example.com"
+            tutor.registration_time = timezone.now()
+            tutor.birthday = timezone.now()
+            tutor.matriculation_number = str(tutor.pk).zfill(8)[:8]
+            tutor.comment = ""
+            tutor.save()
         return True
     return False
 
