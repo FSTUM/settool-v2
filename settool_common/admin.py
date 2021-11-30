734035023c0893d40fc5fe260876e7c7656b7588
Frank Elsinga <frank@elsinga.de>
2021-11-30 23:22:20 +0100

    registered AnonymisationLog in the the admin interface

--- settool_common/admin.py
+++ settool_common/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Semester, Subject
+from .models import AnonymisationLog, Semester, Subject
 
+admin.site.register(AnonymisationLog)
 admin.site.register(Semester)
 admin.site.register(Subject)
