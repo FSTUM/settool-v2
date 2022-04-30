FSTUM/settool-v2

commit 70c68cca303d70118e51befe525fef78331bd84d

changed datetime.datetime to timezone to stop this migration from complaining about TZ-Support

guidedtours/migrations/0005_participant_time.py
@@ -3,6 +3,7 @@
 import datetime
 
 from django.db import migrations, models
+from django.utils import timezone
 
 
 class Migration(migrations.Migration):
@@ -16,7 +17,7 @@ class Migration(migrations.Migration):
             model_name="participant",
             name="time",
             field=models.DateTimeField(
-                default=datetime.datetime.now,
+                default=timezone.now,
                 verbose_name="Registration Time",
             ),
         ),

commit ecd34b04899c0f89d165228ed44e5b21c4770d50

fixed minor formatting issues introduced in #146

fahrt/views/participants_views.py
@@ -278,7 +278,6 @@ def set_status_waitinglist(request: WSGIRequest, participant_pk: UUID) -> HttpRe
 
 @permission_required("fahrt.view_participants")
 def set_payment_deadline(request: WSGIRequest, participant_pk: UUID, weeks: int) -> HttpResponse:
-    weeks = int(weeks)  # save due to regex in urls.py
     if weeks not in [1, 2, 3]:
         raise Http404("Invalid number of weeks")
 

guidedtours/migrations/0005_participant_time.py
@@ -1,7 +1,5 @@
 # Generated by Django 1.9 on 2015-12-21 08:08
 
-import datetime
-
 from django.db import migrations, models
 from django.utils import timezone
 

tutors/templates/tutors/task/view.html
@@ -256,7 +256,9 @@
 {% block defered_script %}
 {% if perms.tutor.edit_tutors %}
 <script type="text/javascript">
-    const initialValues = new Set([{% for tutor in assigned_tutors %} '{{ tutor.id }}', {% endfor %}]);
+    const initialValues = new Set();
+    {% for tutor in assigned_tutors %} initialValues.add('{{ tutor.id }}');
+    {% endfor %}
 
     /* eslint-disable no-unused-vars */
     const assignTutor = (id) => {
@@ -283,7 +285,7 @@
     /* eslint-disable no-unused-vars */
     const unassignTutor = (id) => {
         // form, to be submitted to django; selected will be kept.
-        document.getElementById(`tutor_${id}`).removeAttribute("selected");
+        document.getElementById(`tutor_${id}`).removeAttribute('selected');
         // tutor-unassignent-button
         const src = $(`#tutor_${id}_tr`);
 