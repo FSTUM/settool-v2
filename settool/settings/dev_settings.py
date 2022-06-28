53dc5141db66a471c1d9ca97ae256bd87e41e6de
Frank Elsinga <frank@elsinga.de>
2022-06-28 16:37:15 +0200

    added a sample database configuration to test for differences in prod

--- settool/settings/dev_settings.py
+++ settool/settings/dev_settings.py
@@ -5,3 +5,15 @@ from settool.settings.base_settings import *  # noqa: 401,F403
 USE_KEYCLOAK = False
 
 AUTHENTICATION_BACKENDS = ("django.contrib.auth.backends.ModelBackend",)
+
+# sample database configuration to test for differences in prod
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "password",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     },
+# }
